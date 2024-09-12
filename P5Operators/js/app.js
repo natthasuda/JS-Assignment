84276bfc17e0e518d38db5cbabfe710ea416fb47
natthasuda <nutsuk246@ihmeducation.se>
2024-09-12 14:52:10 +0200

    Add assignment structure

---
+++ P5Operators/js/app.js
@@ -0,0 +1,9 @@
+let speedInput = 72;
+let speedOutput= 0;
+speedOutput = speedInput / 3.6;
+console.log(speedOutput);
+
+let minutesInput= 3;
+let secondOutput = 0;
+minutesInput = secondOutput / ;
+console.log(secondOutput);
