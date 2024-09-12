7242f3805360d873281aa31794d6df8f6b36308c
natthasuda <nutsuk246@ihmeducation.se>
2024-09-12 11:08:03 +0200

    Create number of bottles

---
+++ P4Variables/js/app.js
@@ -0,0 +1,11 @@
+let numberOfBottles = 10;
+const line1 = "green bottles hanging on the wall";
+const line2 = "and if one green bottle, should accidentally fall, there will be";
+
+console.log(numberOfBottles);
+console.log(line1);
+console.log(numberOfBottles);
+console.log(line2);
+console.log(line2);
+console.log(9);
+console.log(line1);
