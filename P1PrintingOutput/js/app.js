1eda3dfba525587ae24272eb89d831b8a6242b32
natthasuda <nutsuk246@ihmeducation.se>
2024-09-12 11:08:17 +0200

    Update

--- P1PrintingOutput/js/app.js
+++ P1PrintingOutput/js/app.js
@@ -1,2 +1,20 @@
 console.log("Hello World!");
 console.log("My name is Nattha");
+console.log("I'm currently learning how to use Javascript");
+// This is how you made a invisible comment without making it appear as a code
+
+let hoursInClass = 5;
+console.log(hoursInClass);
+let hoursOfSleep = 8;
+console.log(hoursOfSleep);
+console.log(hoursOfSleep + hoursInClass);
+
+
+let five = 5;
+console.log(five);
+let eight = 8;
+console.log(eight);
+console.log(five + eight);
+let total = 0;
+total = five + eight;
+
