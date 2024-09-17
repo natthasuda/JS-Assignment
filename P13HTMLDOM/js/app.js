ac4420a886d85564ee34a0fe664052b92a99d4fd
natthasuda <nutsuk246@ihmeducation.se>
2024-09-17 16:24:34 +0200

    Updated

--- P13HTMLDOM/js/app.js
+++ P13HTMLDOM/js/app.js
@@ -1,19 +1,23 @@
 // Reference "text0" and assign it to the variable called "textField0"
-const textField0 = document.getElementById("text0");
-
-// Take textField0 and change to name
-textField0.innerHTML= "Nattha";
-textField0.innerHTML = average(9, 13);
-textField0.innerHTML = multiplyBySelf(6);
-
-
-// Reference "header5" and assign it to the variable called "headerText5"
-const headerText5 = document.getElementById("header5");
-
 // Take headerText5 and change it to "This is my website"
+// Take headerText5, access its style, access its color & change it
+// Take textField0 and change to name
+// Reference "header5" and assign it to the variable called "headerText5"
+
+const textField0 = document.getElementById("text0");
+textField0.innerHTML= "Nattha";
+
+const headerText5 = document.getElementById("header5");
 headerText5.innerHTML= "This is my website"
 
-// Take headerText5, access its style, access its color & change it
+const headerText2 = document.getElementById("header2");
+headerText2.innerHTML= multiplyBySelf(5)
+
+const headerText3 = document.getElementById("header3");
+headerText3.innerHTML= getRandomInt().toString();
+
+
+
 headerText5.style.color = "indigo";
 
 const button0 = document.getElementById("button0");
@@ -28,3 +32,7 @@ function multiplyBySelf(numberToMultiply) {
   return numberToMultiply * numberToMultiply;
 }
 
+function getRandomInt() {
+  return Math.floor(Math.random() * 3);
+
+}
